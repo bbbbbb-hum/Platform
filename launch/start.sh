62a3df7b8432c4f807fbc8e8ec80196970fcdbef
Lucas <lucas7523@163.com>
2025-11-14 16:48:29 +0800

    修正端口检查

--- launch/start.sh
+++ launch/start.sh
@@ -28,7 +28,7 @@ fi
 
 # 2. 检查端口是否被占用（尝试从配置文件读取，失败则使用默认值）
 DEFAULT_PORT="9001"
-
+PORT=$DEFAULT_PORT
 
 
 
@@ -79,7 +79,7 @@ echo
 
 
 # 后台启动服务并保存PID
-nohup "$EXEC_FILE" --env=$ENV 2>>"${LOG_FILE}" 2>&1 &
+nohup "$EXEC_FILE" --env=$ENV >>"${LOG_FILE}" 2>&1 &
 PID=$!
 
 # 保存PID到文件
