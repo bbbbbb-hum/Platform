2e0d11b6a6e6830744c647e0842bc62010d7b6ed
wangcs <296952503@163.com>
2025-12-12 11:03:32 +0800

    去掉分组启动

--- src/models/ae_mcp_services.go
+++ src/models/ae_mcp_services.go
@@ -37,11 +37,9 @@ func (m *AeMcpServices) GetOne(id int32) error {
 	db := GetDB()
 	return db.Where("id = ?", id).First(m).Error
 }
-func (m *AeMcpServices) GetList(serverGroup int64) (err error, list []*AeMcpServices) {
+func (m *AeMcpServices) GetList() (err error, list []*AeMcpServices) {
 	db := GetDB()
 	db = db.Where("is_created = ?", true)
-	// 关掉分组启动，等需要的时候打开
-	//db = db.Where("created_group = ?", serverGroup)
 	err = db.Find(&list).Error
 	return
 }
--- src/servers/server.go
+++ src/servers/server.go
@@ -1,7 +1,6 @@
 package servers
 
 import (
-	"AgentEarth_AgentPlatform/src/helpers/config"
 	"AgentEarth_AgentPlatform/src/helpers/logger"
 	"AgentEarth_AgentPlatform/src/models"
 	"AgentEarth_AgentPlatform/src/servers/task_chain"
@@ -33,7 +32,7 @@ func Initialize() error {
 	logger.Info("开始初始化MCP服务...")
 	// 获取所有MCP服务配置
 	serviceModel := &models.AeMcpServices{}
-	err, serviceList := serviceModel.GetList(config.GetInt64("server.group"))
+	err, serviceList := serviceModel.GetList()
 	if err != nil {
 		return fmt.Errorf("获取MCP服务列表失败: %w", err)
 	}
