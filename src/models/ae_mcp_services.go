2537a70914c7986698f533f4fd440c7128d3b4ee
wangcs <296952503@163.com>
2026-02-13 15:54:36 +0800

    测试一条数据

--- src/models/ae_mcp_services.go
+++ src/models/ae_mcp_services.go
@@ -51,8 +51,8 @@ func (m *AeMcpServices) GetOneByServerId(serverId string) (*AeMcpServices, error
 
 func (m *AeMcpServices) GetList() (err error, list []*AeMcpServices) {
 	db := GetDB()
-	db = db.Where("is_created = ?", true)
-	//db = db.Where("id = ?", 7)
+	//db = db.Where("is_created = ?", true)
+	db = db.Where("id = ?", 336)
 	err = db.Find(&list).Error
 	return
 }
